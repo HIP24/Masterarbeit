b1d358d6b52a4df1412af36709a08920d05658a4
hip24 <halilpamuk24@gmail.com>
2024-03-04 16:53:18 +0100

    corrected after_bitbake_2.sh and working_trace-cmd

--- documentation/yocto/after_bitbake_2.sh
+++ documentation/yocto/after_bitbake_2.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-ip_address="192.168.1.141"
+ip_address="192.168.1.128"
 
-#ssh-keygen -f "/home/sigma_ibo/.ssh/known_hosts" -R "$ip_address"
 #cd /home/sigma_ibo/Develop/Yocto_local/salamander/salamander-core2/build/tmp/deploy/ipk/core2-64
 #ssh root@"$ip_address" 'mkdir bb'
-ssh-keygen
+ssh-keygen -f "/home/sigma_ibo/.ssh/known_hosts" -R "$ip_address"
 ssh-copy-id root@"$ip_address"
 scp /usr/local/bin/trace-cmd root@"$ip_address":/usr/bin
 scp /usr/local/lib64/libtracefs.so.1 root@"$ip_address":/lib64
