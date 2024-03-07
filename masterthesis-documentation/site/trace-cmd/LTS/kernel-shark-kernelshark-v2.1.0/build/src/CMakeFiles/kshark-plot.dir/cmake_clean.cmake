a3cb3c95e9e0412146a0a7302d9d33b9c453c6a2
HIP24 <43355388+HIP24@users.noreply.github.com>
2024-03-07 17:19:40 +0100

    site

---
+++ masterthesis-documentation/site/trace-cmd/LTS/kernel-shark-kernelshark-v2.1.0/build/src/CMakeFiles/kshark-plot.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../../lib/libkshark-plot.pdb"
+  "../../lib/libkshark-plot.so.2.1.0"
+  "CMakeFiles/kshark-plot.dir/KsPlotTools.cpp.o"
+  "CMakeFiles/kshark-plot.dir/KsPlotTools.cpp.o.d"
+  "CMakeFiles/kshark-plot.dir/KsPlugins.cpp.o"
+  "CMakeFiles/kshark-plot.dir/KsPlugins.cpp.o.d"
+  "CMakeFiles/kshark-plot.dir/libkshark-plot.c.o"
+  "CMakeFiles/kshark-plot.dir/libkshark-plot.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/kshark-plot.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
