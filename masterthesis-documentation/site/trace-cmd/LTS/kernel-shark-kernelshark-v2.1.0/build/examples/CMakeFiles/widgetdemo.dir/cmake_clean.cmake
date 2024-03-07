38461a34131bd2a363d1b5060db4def19e3e7bf6
HIP24 <43355388+HIP24@users.noreply.github.com>
2024-03-07 10:30:20 +0100

    rename mastherthesis-documentation

---
+++ masterthesis-documentation/site/trace-cmd/LTS/kernel-shark-kernelshark-v2.1.0/build/examples/CMakeFiles/widgetdemo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../bin/widgetdemo"
+  "../../bin/widgetdemo.pdb"
+  "CMakeFiles/widgetdemo.dir/widgetdemo.cpp.o"
+  "CMakeFiles/widgetdemo.dir/widgetdemo.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/widgetdemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
