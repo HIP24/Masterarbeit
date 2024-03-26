4c5e53f9356f9e57a35dbd9e02a732adbeac405f
HIP24 <your_preferred_email@example.com>
2024-03-26 14:43:01 +0100

    plot python

---
+++ masterthesis-documentation/docs/sigmatek/xenomai/print_numbers.py
@@ -0,0 +1,9 @@
+# Open the file in write mode
+with open("numbers.txt", "w") as file:
+    # Write numbers from 1 to 600
+    for number in range(1, 601):
+        file.write(str(number) + "\n")
+
+# Print a success message
+print("The numbers from 1 to 600 are successfully saved to numbers.txt file.")
+
