18981849c8f42299779b5d1da65ce8784431d862
HIP24 <your_preferred_email@example.com>
2024-04-25 15:35:37 +0200

    report.sh

--- masterthesis-documentation/docs/sigmatek/trace-cmd/analysis/test/remove.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm guest_report.txt kvm_exit_count.png results_guest_report.md results_host_report.md host_report.txt   kvm_exit_count.txt results_guest_report.png results_host_report.png && echo "deleted all"
