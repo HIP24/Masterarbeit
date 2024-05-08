5721eb208d1887b2dff62f94f8de959d5e3883f3
HIP24 <halilpamuk24@gmail.com>
2024-05-08 09:33:39 +0200

    ps

--- masterthesis-documentation/docs/tools/ps/process_tree.sh
+++ masterthesis-documentation/docs/tools/ps/process_tree.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 python process-tree.py
-mkdir process-tree_generated
+if [ ! -d "process-tree_generated" ]; then
+  mkdir process-tree_generated
+fi
 mv process-tree.md process-tree_generated
 
 
