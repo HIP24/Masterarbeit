d2ce521e869410436d48cc1e91eae34219a91038
HIP24 <halilpamuk24@gmail.com>
2024-08-29 03:57:06 +0200

    remove_urldate.py

---
+++ remove_urldate.py
@@ -0,0 +1,18 @@
+import re
+
+def process_bib_file(input_file, output_file):
+    with open(input_file, 'r') as file:
+        lines = file.readlines()
+
+    with open(output_file, 'w') as file:
+        entry_type = None
+        for line in lines:
+            if line.startswith('@'):
+                entry_type = line.split('{')[0].strip()
+            if entry_type != '@misc' and 'urldate' in line:
+                continue
+            file.write(line)
+
+input_file = 'Literature.bib'
+output_file = 'Literature.bib'
+process_bib_file(input_file, output_file)
